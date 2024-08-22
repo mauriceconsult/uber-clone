e8605b3875c9ef1e7330562d28f55daad962e027
Maurice Consultant <mainna62@outlook.com>
2024-08-22 12:42:55 +0300

    'initialize signup'

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,12 @@
-/* eslint-disable prettier/prettier */
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
 
-const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  );
+const Page = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Home;
+export default Page;
